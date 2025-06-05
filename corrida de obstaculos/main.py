b6e7eaaef7bc1c46af54512945c03dcd57de0eb0
João Victor Bordignon <joaovictorbordignon@gmail.com>
2025-06-05 16:15:15 -0300

    iniciando

---
+++ corrida de obstaculos/main.py
@@ -0,0 +1,13 @@
+import os
+import json
+import time
+import tkinter as tk
+import random
+import pygame
+from tkinter import messagebox
+from recursos.funcoes import inicializarBancoDeDados
+from recursos.funcoes import escreverDados
+
+pygame.init()
+inicializarBancoDeDados()
+tamanho = (1000,700)
